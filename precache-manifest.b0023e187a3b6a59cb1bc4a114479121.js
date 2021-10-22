4c0dd7ed7c230c2af94bb232d45072eb8292de9f
Code Sandbox <hello@codesandbox.io>
2021-10-22 21:36:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.b0023e187a3b6a59cb1bc4a114479121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e97e4061b5f198a0f89fc924709d7",
+    "url": "https://seanmodd.github.io/csb-7ryke/index.html"
+  },
+  {
+    "revision": "e175d5e596fe932f8ddb",
+    "url": "https://seanmodd.github.io/csb-7ryke/static/js/2.28bc19fb.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://seanmodd.github.io/csb-7ryke/static/js/2.28bc19fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2b96763b48adbcf1a1",
+    "url": "https://seanmodd.github.io/csb-7ryke/static/js/main.03e2ad2c.chunk.js"
+  },
+  {
+    "revision": "f598a939699185231637",
+    "url": "https://seanmodd.github.io/csb-7ryke/static/js/runtime-main.d5aba7d4.js"
+  }
+]);
